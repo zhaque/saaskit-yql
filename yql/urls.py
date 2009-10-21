9bee4b0498a9ad951c3da7e781bdd0c599359675
Stepan Rakhimov <strakh@macbook.local>
2009-10-21 16:22:27 +0400

    refactoring, updated README

--- yql/urls.py
+++ yql/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'yql.views.queries', name='yql-query-list'),
-#    url(r'^(?P<slug>[-\w]+)/$', 'livesearch.views.search', name='livesearch_results'),
 )
